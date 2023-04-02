bfaec9ed38b1624a5e583455a8f8a34790295390
Firdavs Karimov <129647008+FirdavsKarimov@users.noreply.github.com>
2023-04-02 11:46:31 +0500

    Add files via upload
    datatypes

---
+++ first/main.js
@@ -0,0 +1,25 @@
+// object --- array , obj , function
+// butun javascriptni o`zi object 
+
+
+// object --- fura yoki containerdek narsa
+
+let ar =[
+'Firdavs' , 12, 'Karimov'
+]; // ma`lumotni index siga qarab chaqiradi
+// arrayda umumiy ma`lumot yoziladi 
+
+let obj = {
+ name : 'Firdavs',
+ age : 21
+}
+
+let func = ()=>{
+
+};
+ function text(){
+
+ } // ma`lum bir kodlar to`plami
+
+ console.log(ar[0])
+ console.log(obj)
