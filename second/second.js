c36170214694c5bea36afdc4c1abbe5ae851d916
Firdavs Karimov <129647008+FirdavsKarimov@users.noreply.github.com>
2023-04-02 11:50:42 +0500

    Type conversion Operators , Comparision

---
+++ second/second.js
@@ -0,0 +1,59 @@
+// // let a = 1 ;
+// // let b = '1';
+// // console.log(a+b);
+
+
+// //==============
+
+// let a = "10";
+// let b = "10. 3" ;
+// console. log (Number (a) );
+// console. log (Number. parseInt (a) ); // butun qism yaxlit qism 
+// console. log (Number.parseFloat (a)); // 10lik sonlar kasr qisim bilan birga olinadi bu number bilan bir xil holatda
+// console.log(parseInt(b)); // parseint va parsefloatlarni numbersiz ham yozsa bo`ladi 
+// console.log(parseFloat(b)); 
+//==========================
+
+
+// true = !0 , null , underfined;
+// console.log(true ==1); // true
+// console.log(false == 0); // true
+// console.log(false == null) ; // false
+// console.log(undefined == null); // true
+// console.log(false == undefined) // false
+
+ 
+//  // ====================
+ 
+//  let a = 11 ;
+//  console.log(a++) // 11
+// console.log(a) //12
+// console.log(++a) // 13
+
+//=====================
+// OBRIVATIONS
+ 
+// let a = 50
+// a = a+10 // 50 + 10
+// a+=10 // 60 + 10
+// += ; /= ; -= ; va hokazolar ishlatiladi 
+
+// COMPARITIONS == taqqoslash  qiymati doim boolean yani true falsega qaytadi 
+ 
+// let a = 10
+// let b = 11
+// let c = 11
+ // > ; < ; == ; === ; <= ; >= ; !-ignor   
+// console.log(a > b)
+
+// ========== second lesson
+let a = 2;
+let x = 1 + (a*=2)
+let b = '' - 1 +0
+console.log(b)
+
+
+console.log('2' * '3')
+
+
+
